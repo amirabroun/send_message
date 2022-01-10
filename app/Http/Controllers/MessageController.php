7a4086dcb2ca991c2658c71e6dd7e1cb1b2943a8
amirabroun <abroon234@gmail.com>
2022-01-10 11:19:30 +0330

    Add model file User, Message to project

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,35 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Models\Message;
 
 class MessageController extends Controller
 {
     public function index()
     {
-        $messages = $this->getMessage();
+        $messages = Message::getMessage();
 
         return view('message', compact($messages, 'messages'));
     }
 
     public function messageRequest(Request $request)
     {
-        $this->selectMessage($request->input('id'));
+        Message::selectMessage($request->input('id'));
 
-        $messages = $this->getMessage();
+        $messages = Message::getMessage();
 
         return view('message', compact($messages, 'messages'));
-    }
-
-    public function selectMessage($id)
-    {
-        DB::table('messages')->update(['status' => '0']);
-
-        DB::table('messages')->where('id', '=', $id)->update(['status' => '1']);
-    }
-
-    public function getMessage()
-    {
-        return DB::table('messages')->get();
     }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Message extends Model
 {
     use HasFactory;
+
+    public static function selectMessage($id)
+    {
+        DB::table('messages')->update(['status' => '0']);
+
+        DB::table('messages')->where('id', '=', $id)->update(['status' => '1']);
+    }
+
+    public static function getMessage($status = null)
+    {
+        if ($status) {
+            return (DB::table('messages')->where('status', '=', $status)->first()->message);
+        }
+
+        return DB::table('messages')->get();
+    }
 }
